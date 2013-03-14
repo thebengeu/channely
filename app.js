aad32677122f3d2f5094776eb000add95d800cb4
ahbeng <ah@beng.me>
2013-03-15 02:09:29 +0800

    Mongoosify stuff

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var http = require('http');
-var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 var path = require('path');
-
-var ObjectID = mongodb.ObjectID;
 
 var app = express();
 
@@ -21,63 +19,61 @@ app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
-mongodb.MongoClient.connect('mongodb://127.0.0.1:3002/meteor', function (err, db) {
-  if (err) {
-    return console.dir(err);
-  }
+mongoose.connect('mongodb://127.0.0.1:3002/meteor');
 
+var timelineSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+var Timeline = mongoose.model('Timeline', timelineSchema)
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
   // Single file for now, may want to split to routes/timeline.js etc. next time.
   app.get('/timelines', function (req, res) {
-    db.collection('timelines').find().toArray(function (err, items) {
-      res.json(items);
+    Timeline.find(function (err, timelines) {
+      err ? res.send(500, err) : res.json(timelines);
     });
   });
 
   app.get('/timelines/:id', function (req, res) {
-    db.collection('timelines').findOne({
-      _id: new ObjectID(req.params.id)
-    }, function (err, item) {
-      res.json(item);
+    Timeline.findById(req.params.id, function (err, timeline) {
+      !timeline ? res.send(404) : res.json(timeline);
     });
   });
 
   app.post('/timelines', function (req, res) {
-    db.collection('timelines').insert(req.body, {w: 1}, function (err, result) {
-      res.json(result[0]);
+    var timeline = new Timeline(req.body);
+    timeline.save(function (err) {
+      err ? res.send(422, err) : res.send(timeline);
     });
   });
 
   app.put('/timelines/:id', function (req, res) {
-    db.collection('timelines').update({
-        _id: new ObjectID(req.params.id)
-      },
-      req.body,
-      {w: 1},
-      function (err, result) {
-        if (err) {
-          res.send(500, err);
-        } else if (result === 0) {
-          res.send(404);
-        } else {
-          res.send(204);
-        }
-      });
+    Timeline.findById(req.params.id, function (err, timeline) {
+      if (!timeline) {
+        res.send(404);
+      } else {
+        timeline.name = req.body.name;
+        timeline.save(function (err) {
+          err ? res.send(422, err) : res.send(timeline);
+        });
+      }
+    });
   });
 
   app.delete('/timelines/:id', function (req, res) {
-    db.collection('timelines').remove({
-        _id: new ObjectID(req.params.id)
-      },
-      {w: 1},
-      function (err, result) {
-        if (err) {
-          res.send(500, err);
-        } else if (result === 0) {
-          res.send(404);
-        } else {
+    Timeline.findById(req.params.id, function (err, timeline) {
+      if (!timeline) {
+        res.send(404);
+      } else {
+        timeline.remove(function () {
           res.send(204);
-        }
-      });
+        });
+      }
+    });
   });
 });
 
