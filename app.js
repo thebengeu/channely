a161d692fa47c78b545e57efc0e05978f637605a
Eli James <ejames@pandamian.com>
2013-03-15 10:43:41 +0800

    Refactored timeline routes into routes/

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var http = require('http');
 var mongoose = require('mongoose');
 var path = require('path');
 
+// Routing imports
+var timelines = require('./routes/timelines');
+
 var app = express();
 
 app.configure(function () {
@@ -24,50 +27,12 @@ mongoose.connect('mongodb://127.0.0.1:3002/meteor');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  // Single file for now, may want to split to routes/timeline.js etc. next time.
-  app.get('/timelines', function (req, res) {
-    Timeline.find(function (err, timelines) {
-      err ? res.send(500, err) : res.json(timelines);
-    });
-  });
 
-  app.get('/timelines/:id', function (req, res) {
-    Timeline.findById(req.params.id, function (err, timeline) {
-      !timeline ? res.send(404) : res.json(timeline);
-    });
-  });
-
-  app.post('/timelines', function (req, res) {
-    var timeline = new Timeline(req.body);
-    timeline.save(function (err) {
-      err ? res.send(422, err) : res.send(timeline);
-    });
-  });
-
-  app.put('/timelines/:id', function (req, res) {
-    Timeline.findById(req.params.id, function (err, timeline) {
-      if (!timeline) {
-        res.send(404);
-      } else {
-        timeline.name = req.body.name;
-        timeline.save(function (err) {
-          err ? res.send(422, err) : res.send(timeline);
-        });
-      }
-    });
-  });
-
-  app.delete('/timelines/:id', function (req, res) {
-    Timeline.findById(req.params.id, function (err, timeline) {
-      if (!timeline) {
-        res.send(404);
-      } else {
-        timeline.remove(function () {
-          res.send(204);
-        });
-      }
-    });
-  });
+  app.get('/timelines', timelines.index);
+  app.get('/timelines/:id', timelines.show);
+  app.post('/timelines', timelines.create);
+  app.put('/timelines/:id', timelines.update);
+  app.delete('/timelines/:id', timelines.delete);
 });
 
 http.createServer(app).listen(app.get('port'), function () {
---
+++ routes/timelines.js
@@ -0,0 +1,47 @@
+var Timeline = require('../models/timeline').Timeline;
+
+// Timeline routes
+
+exports.index = function (req, res) {
+    Timeline.find(function (err, timelines) {
+      err ? res.send(500, err) : res.json(timelines);
+    });
+}
+
+exports.show = function (req, res) {
+    Timeline.findById(req.params.id, function (err, timeline) {
+      !timeline ? res.send(404) : res.json(timeline);
+    });
+}
+
+exports.create = function (req, res) {
+    var timeline = new Timeline(req.body);
+    timeline.save(function (err) {
+      err ? res.send(422, err) : res.send(timeline);
+    });
+}
+
+exports.update = function (req, res) {
+    Timeline.findById(req.params.id, function (err, timeline) {
+      if (!timeline) {
+        res.send(404);
+      } else {
+        timeline.name = req.body.name;
+        timeline.save(function (err) {
+          err ? res.send(422, err) : res.send(timeline);
+        });
+      }
+    });
+}
+
+exports.delete = function (req, res) {
+    Timeline.findById(req.params.id, function (err, timeline) {
+      if (!timeline) {
+        res.send(404);
+      } else {
+        timeline.remove(function () {
+          res.send(204);
+        });
+      }
+    });
+}
