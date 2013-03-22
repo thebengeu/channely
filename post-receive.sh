4869106faa61db709008aa401a212936ab145b9f
ahbeng <ah@beng.me>
2013-03-22 18:15:43 +0800

    rm -rf bundle before untar, use correct bundle dir

--- post-receive.sh
+++ post-receive.sh
@@ -4,8 +4,9 @@ cd /ebs/channely/meteor
 mrt bundle ../channely.tgz
 cd /ebs/channely
 npm install
+rm -rf bundle
 tar -zxvf channely.tgz
 forever stop main.js
-PORT=10001 MONGO_URL=mongodb://127.0.0.1:27017/meteor ROOT_URL=http://upthetreehouse.com forever start -a -l meteor_forever.log -o meteor_out.log -e meteor_err.log --sourceDir /ebs/channely/meteor/bundle main.js
+PORT=10001 MONGO_URL=mongodb://127.0.0.1:27017/meteor ROOT_URL=http://upthetreehouse.com forever start -a -l meteor_forever.log -o meteor_out.log -e meteor_err.log --sourceDir /ebs/channely/bundle main.js
 forever stop app.js
 PORT=10000 MONGO_URL=mongodb://127.0.0.1:27017/meteor forever start -a -l node_forever.log -o node_out.log -e node_err.log --sourceDir /ebs/channely app.js
