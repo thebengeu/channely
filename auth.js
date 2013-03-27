ac932072e19b777e9cdb3478459ce87126f60102
Eli James <ejames@pandamian.com>
2013-03-28 00:07:09 +0800

    Remove clientId field in User
    clientId should map to user Id instead.

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ var passport = require('passport'),
 
 passport.use(new ClientPasswordStrategy(
       function(clientId, clientSecret, done) {
-        User.find({ clientId: clientId }, function (err, client) {
+        User.findById(clientId, function (err, client) {
           if (err) { return done(err); }
           if (!client) { return done(null, false); }
           if (client.clientSecret != clientSecret) { return done (null, false); }
--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ var mongoose = require('mongoose'),
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, index: { unique: true} },
     password: { type: String, required: true},
-    clientId: { type: String },
     clientSecret: { type: String }
 });
 
