af1577ec2c753ffb90c797fb07533367cb3709fe
Eli James <ejames@pandamian.com>
2013-03-30 22:07:38 +0800

    Added owner and username fields to Imagepost.

--- models/imagepost.js
+++ models/imagepost.js
@@ -1,12 +1,13 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
+var mongoose = require('mongoose'), 
+    Schema = mongoose.Schema;
 
 var imagePostSchema = new mongoose.Schema({
-  // owner
   _channel: { type: String, ref:'Channel' },
   time: { type: Date, default: Date.now },
   content: { type: String, trim: true },
-  url: { type: String, required: true, trim: true }
+  url: { type: String, required: true, trim: true },
+  owner: { type: String, ref: 'User' },
+  username: { type: String }
 });
 
 var imagePostModel = mongoose.model('ImagePost', imagePostSchema);
