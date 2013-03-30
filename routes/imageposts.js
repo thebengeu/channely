2749599b6663595f184982a04eccc1b3e88db715
Eli James <ejames@pandamian.com>
2013-03-30 22:22:56 +0800

    Protect Imagepost endpoints, apply user ownership and authorization for deletion.

--- routes/imageposts.js
+++ routes/imageposts.js
@@ -1,37 +1,63 @@
-var ImagePost = require('../models/imagepost').ImagePost;
-var Channel = require('../models/channel').Channel;
+var ImagePost = require('../models/imagepost').ImagePost,
+    Channel = require('../models/channel').Channel,
+    User = require('../models/user').User,
+    passport = require('passport');
 
 exports.index = function (req, res) {
   ImagePost.find({ _channel: req.params.id }, null, {sort: {time: 1}},
     function(err, imagePosts) {
       err ? res.send(500, err) : res.json(imagePosts);
     });
-}
+};
 
 exports.create = function (req, res) {
   Channel.findById(req.body.channelID, function (err, channel) {
-    if (!err && !channel) res.send(404, "No such channel exists!");
-    else if (err) res.send(500, err);
+    if (err) { res.send(500, err); }
+    else if (!channel) { res.send(404, "No such channel exists!"); }
     else {
-      var imagePost = new ImagePost({
+      var imageProperties = {
         content: req.body.content,
         url: req.body.url,
-        _channel: channel._id
-      });
-      imagePost.save(function (err) {
-        err ? res.send(422, err) : res.send(201, imagePost);
-      });
-    }
-  });
-}
+        _channel: channel._id };
 
-exports.delete = function (req, res) {
-  ImagePost.findById(req.params.id, function (err, imagePost) {
-    if (!imagePost) res.send(404);
-    else {
-      imagePost.remove(function () {
-        res.send(204);
-      });
+      // if there's an access token, get the user and attach it to this post
+      // otherwise just save a username
+      if (req.query.access_token) {
+        User.findOne({accessToken: req.query.access_token },
+          function(err, user){
+            if (err || !user) {
+              imageProperties.username = req.body.username;
+            } else if (user) {
+              imageProperties.owner = user._id;
+            }
+
+            var imagePost = new ImagePost(imageProperties);
+            imagePost.save(function (err) {
+              err ? res.send(422, err) : res.send(201, imagePost);
+            });  
+          });
+      } else {
+        imageProperties.username = req.body.username;
+        var imagePost = new ImagePost(imageProperties);
+        imagePost.save(function (err) {
+          err ? res.send(422, err) : res.send(201, imagePost);
+        });
+      }
     }
   });
-}
+};
+
+exports.delete = [
+  passport.authenticate('bearer', {session: false}),
+  function (req, res) {
+    ImagePost.findById(req.params.id, function (err, imagePost) {
+      if (!imagePost) { res.send(404); }
+      else if (imagePost.owner != req.user._id) { res.send(403); }
+      else {
+        imagePost.remove(function () {
+          res.send(204);
+        });
+      }
+    });
+  }
+];
