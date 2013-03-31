87333229749eeb4c369e41a0ac1629bc77f48b4b
Eli James <ejames@pandamian.com>
2013-03-31 14:31:42 +0800

    Fix oauth2.js bugs - access token dance.

--- oauth2.js
+++ oauth2.js
@@ -16,29 +16,20 @@ var oauth2orize = require('oauth2orize'),
 // NOTE: in actual fact, client secret is supposed to be used to calculate
 // a nonce, but considering our API is not graded, CBA.
 exports.token = [
-  passport.authenticate(['basic'], { session: false }),
+passport.authenticate(['basic'], { session: false }),
   function(req, res) {
     Client.findById(req.body.clientId, function (err, client) {
       if (!client) { res.send(401, "No such client"); }
-      if (!client.clientSecret == req.body.clientSecret) res.send(401);
+      if (client.clientSecret != req.body.clientSecret) res.send(401);
     });
-    User.findOne({ username: req.body.username }, function (err, user) {
-      if (!user) { res.send(401, "No such user"); }
-      user.comparePassword(req.body.password, function(err, isMatch) {
-        if (err) res.send(500, err);
-        if (isMatch) {
-          user.accessToken = utils.randomStr(256);
-          user.save(function (err) {
-            err? res.send(422, err) : res.json(200, {
-              username: user.username,
-              userId: user._id,
-              accessToken: user.accessToken });
-          });
-        } else {
-          res.send(401);
-        }
-      });
+    var user = req.user;
+    user.accessToken = utils.randomStr(256);
+    user.save(function (err) {
+      err? res.send(422, err) : res.json(200, {
+        username: user.username,
+        userId: user._id,
+        accessToken: user.accessToken });
     });
   }
-  ]
+]
 
